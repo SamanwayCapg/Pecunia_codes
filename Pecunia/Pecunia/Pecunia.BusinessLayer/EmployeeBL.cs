3a0c41811d3ad856f78a9682aa936db2785e62f7
aishwaryasarnacapgemini <aishwaryasarna_capgemini@outlook.com>
2019-09-16 18:20:13 +0530

    update

--- Pecunia/Pecunia/Pecunia.BusinessLayer/EmployeeBL.cs
+++ Pecunia/Pecunia/Pecunia.BusinessLayer/EmployeeBL.cs
@@ -12,63 +12,70 @@ namespace Pecunia.BusinessLayer
 {
     public class EmployeeBL
     {
+        public static bool EmployeeLogInBL(Employee employee)
+        {
+            bool employeeLoggedIn = false;
+            try
+            {
+                EmployeeDAL employeeDAL = new EmployeeDAL();
+                employeeLoggedIn = employeeDAL.EmployeeLogInDAL(employee);
+            }
+            catch (Exception ex)
+            {
+
+                throw new PecuniaException(ex.Message);
+            }
+
+            return employeeLoggedIn;
+        }
+
         private static bool ValidateEmployee(Employee employee)
         {
             StringBuilder sb = new StringBuilder();
             bool validEmployee = true;
-            if (Regex.IsMatch(employee.EmployeeID, "[EMP][0-9]{14}$") == true) 
+
+            if (Regex.IsMatch(employee.EmployeeID, "[EMP][0-9]{14}$") != true)   //employeeID should start from "EMP" followed by digits and total length 14.
             {
                 validEmployee = false;
                 sb.Append(Environment.NewLine + "Invalid Employee ID");
 
             }
-            if (employee.EmployeeName == string.Empty)
+            if (employee.EmployeeName == " ")               //employee name should not be empty
             {
                 validEmployee = false;
                 sb.Append(Environment.NewLine + "Employee Name Required");
 
-            }
-
-            Regex regexName = new Regex("^[a-zA-Z ]{2,30}*$");
-            if (regexName.IsMatch(employee.EmployeeName) != true)
+            }           
+            if (Regex.IsMatch(employee.EmployeeName, "^[a-zA-Z ]{2,30}*$") != true)        //employee name can consist of only first name space and last name with total length upto 30 
             {
                 validEmployee = false;
                 sb.Append(Environment.NewLine + "Employee Name should be between 2 to 30 characters");
 
             }
-
-            Regex regexCode = new Regex("^[a-zA-Z0-9]+$"); //no spaces allowed
-            if (regexCode.IsMatch(employee.EmployeeCode) != true)
+            if (Regex.IsMatch(employee.EmployeeCode, "[EMC][0-9]{14}$") != true)     //employee code should start from "EMC" followed by digits and total length 14.  
             {
                 validEmployee = false;
-                sb.Append(Environment.NewLine + "Employee Code should be alpha numeric");
+                sb.Append(Environment.NewLine + "Employee Code should be valid");
 
-            }
-
-            Regex regexEmail = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
-            if (regexEmail.IsMatch(employee.EmployeeEmail) != true)
+            }          
+            if (Regex.IsMatch(employee.EmployeeEmail, @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$") != true) //employee email must consist of letters, numbers with @ and a domain name.
             {
                 validEmployee = false;
                 sb.Append(Environment.NewLine + "Employee Email should be valid");
 
-            }
-
-            Regex regexPassword = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$");
-            if (regexPassword.IsMatch(employee.EmployeePassword) != true)
-            {
+            }            
+            if (Regex.IsMatch(employee.EmployeePassword, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$") != true)    //employee password must consist of atleast one small letter, one capital letter, special character and digits with total length upto 8-10
+            {                
                 validEmployee = false;
                 sb.Append(Environment.NewLine + "Employee Password should be valid");
 
             }
-
-            Regex regexMobile = new Regex("^([9]{1})([234789]{1})([0-9]{8})$");
-            if (regexMobile.IsMatch(employee.EmployeeMobile) != true)
+            if (Regex.IsMatch(employee.EmployeeMobile, "^([9]{1})([234789]{1})([0-9]{8})$") != true)   //employee mobile number should be only digits and starting with "91" with total length upto 10 
             {
                 validEmployee = false;
                 sb.Append(Environment.NewLine + "Employee Mobile should be valid");
 
             }
-
             if (validEmployee == false)
                 throw new PecuniaException(sb.ToString());
             return validEmployee;
@@ -82,7 +89,7 @@ namespace Pecunia.BusinessLayer
             {
                 if (ValidateEmployee(newEmployee))
                 {
-                    EmployeeDAL employeeDAL = new EmployeeDAL();
+                    EmployeeDAL employeeDAL = new EmployeeDAL();         //calling DAL class method to add employee
                     employeeAdded = employeeDAL.AddEmployeeDAL(newEmployee);
                 }
             }
@@ -103,7 +110,7 @@ namespace Pecunia.BusinessLayer
             List<Employee> employeeList = null;
             try
             {
-                EmployeeDAL employeeDAL = new EmployeeDAL();
+                EmployeeDAL employeeDAL = new EmployeeDAL();        //calling DAL class method to list employees
                 employeeList = employeeDAL.GetAllEmployeesDAL();
             }
             catch (PecuniaException ex)
@@ -122,7 +129,7 @@ namespace Pecunia.BusinessLayer
             Employee searchEmployee = null;
             try
             {
-                EmployeeDAL employeeDAL = new EmployeeDAL();
+                EmployeeDAL employeeDAL = new EmployeeDAL();    //calling DAL class method to search employees
                 searchEmployee = employeeDAL.SearchEmployeeDAL(searchEmployeeID);
             }
             catch (PecuniaException ex)
@@ -144,7 +151,7 @@ namespace Pecunia.BusinessLayer
             {
                 if (ValidateEmployee(updateEmployee))
                 {
-                    EmployeeDAL employeeDAL = new EmployeeDAL();
+                    EmployeeDAL employeeDAL = new EmployeeDAL();       //calling DAL class method to update employees
                     employeeUpdated = employeeDAL.UpdateEmployeeDAL(updateEmployee);
                 }
             }
@@ -165,10 +172,10 @@ namespace Pecunia.BusinessLayer
             bool employeeDeleted = false;
             try
             {
-                if (Regex.IsMatch(deleteEmployeeID, "[EMP][0-9]{14}$") == true)
+                if (Regex.IsMatch(deleteEmployeeID, "[EMP][0-9]{14}$") == true)   //matching user provided employeeID with employeeID in list
                 {
                     EmployeeDAL employeeDAL = new EmployeeDAL();
-                    employeeDeleted = employeeDAL.DeleteEmployeeDAL(deleteEmployeeID);
+                    employeeDeleted = employeeDAL.DeleteEmployeeDAL(deleteEmployeeID);   //if matched, the employee is deleted by calling DAL class method
                 }
                 else
                 {
